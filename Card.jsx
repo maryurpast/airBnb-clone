57421842e190cebd5d93e66c7d25b1a9b1c25b02
maryurpast <maryurpast@gmail.com>
2023-07-29 01:30:33 +0300

    Moved images into dist directory

--- Card.jsx
+++ Card.jsx
@@ -13,13 +13,13 @@ export default function Card(props) {
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
       <img
-        src={`./images/${props.coverImg}`}
+        src={`./dist/images/${props.coverImg}`}
         alt="Photo"
         className="card--image"
       />
 
       <div className="card--stats">
-        <img src="./images/star.png" alt="star" className="card--star" />
+        <img src="./dist/images/star.png" alt="star" className="card--star" />
         <span>{props.stats.rating}</span>
         <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
--- Hero.jsx
+++ Hero.jsx
@@ -4,15 +4,15 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="hero--grid">
-        <img src="./images/image1.png" alt="image" className="image1" />
-        <img src="./images/image2.png" alt="image" className="image2" />
-        <img src="./images/image3.png" alt="image" className="image3" />
-        <img src="./images/image4.png" alt="image" className="image4" />
-        <img src="./images/image5.png" alt="image" className="image5" />
-        <img src="./images/image6.png" alt="image" className="image6" />
-        <img src="./images/image7.png" alt="image" className="image7" />
-        <img src="./images/image8.png" alt="image" className="image8" />
-        <img src="./images/image9.png" alt="image" className="image9" />
+        <img src="./dist/images/image1.png" alt="image" className="image1" />
+        <img src="./dist/images/image2.png" alt="image" className="image2" />
+        <img src="./dist/images/image3.png" alt="image" className="image3" />
+        <img src="./dist/images/image4.png" alt="image" className="image4" />
+        <img src="./dist/images/image5.png" alt="image" className="image5" />
+        <img src="./dist/images/image6.png" alt="image" className="image6" />
+        <img src="./dist/images/image7.png" alt="image" className="image7" />
+        <img src="./dist/images/image8.png" alt="image" className="image8" />
+        <img src="./dist/images/image9.png" alt="image" className="image9" />
       </div>
       <h1 className="hero--header">Online Experiences</h1>
       <p className="hero--text">
